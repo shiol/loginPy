ff8f0920699148609150ba2ec9cf12e1742b85c7
shiol <shiol003@gmail.com>
2021-12-10 07:52:25 -0300

    position

--- index.py
+++ index.py
@@ -20,17 +20,17 @@ logo = PhotoImage(file="logo.png")
 logolabel = Label(leftframe, image=logo, bg="lightblue")
 logolabel.place(x=50, y=100)
 
-userlabel = Label(rightframe, text="user:", font=("Arial", 20), background="gray", foreground="white")
-userlabel.place(x=10, y=10)
+userlabel = Label(rightframe, text="user:", font=("Arial", 15), background="gray", foreground="white")
+userlabel.place(x=10, y=20)
 
 userentry = ttk.Entry(rightframe, width=30)
-userentry.place(x=100, y=20)
+userentry.place(x=150, y=20)
 
-passwordlabel = Label(rightframe, text="password:", font=("Arial", 20), background="gray", foreground="white")
-passwordlabel.place(x=10, y=100)
+passwordlabel = Label(rightframe, text="password:", font=("Arial", 15), background="gray", foreground="white")
+passwordlabel.place(x=10, y=50)
 
 passwordentry = ttk.Entry(rightframe, width=30,show="*")
-passwordentry.place(x=10, y=140)
+passwordentry.place(x=150, y=50)
 
 loginbutton = ttk.Button(rightframe, text="login", width=10)
 loginbutton.place(x=10, y=200)
